3fd750786e20a8aa973ee00e2d0ed0399ae8ee41
The Choplin Group <thechoplingroup@openclaw-mac.local>
2026-03-17 10:44:07 -0500

    fix: clear instrumentation.ts Sentry imports

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,8 +1,2 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    await import("../sentry.server.config");
-  }
-  if (process.env.NEXT_RUNTIME === "edge") {
-    await import("../sentry.edge.config");
-  }
-}
+// Sentry disabled
+export async function register() {}
